f775f0e09024e63cfde6a3ed3f59cca0762da7e0
Thomas <thomas.1993.smith@gmail.com>
2017-01-18 17:19:14 +1100

    added some basic game logic

--- app.js
+++ app.js
@@ -9,12 +9,41 @@ GAME RULES:
 
 */
 
-var scores, roundScore, activePlayer, dice;
+ var scores, roundScore, activePlayer;
 
-scores = [0,0];
-roundScore = 0;
-activePlayer = 1;
+ scores = [0,0];
+ roundScore = 0;
+ activePlayer = 0;
 
-dice = Math.floor(Math.random() * 6) + 1;
+ //document.querySelector('#current-' + activePlayer).textContent = dice;
+ document.querySelector('.dice').style.display = 'none';
+ document.getElementById('score-0').textContent = 0;
+ document.getElementById('score-1').textContent = 0;
+ document.getElementById('current-0').textContent = 0;
+ document.getElementById('current-1').textContent = 0;
 
-document.querySelector('#current-' + activePlayer).textContent = dice;
+ document.querySelector('.btn-roll').addEventListener('click', function(){
+  // Random Number
+   var dice = Math.floor(Math.random() * 6) + 1;
+  // display the result
+  var diceDOM = document.querySelector('.dice');
+  diceDOM.style.display = 'block';
+  diceDOM.src = 'dice-' + dice + '.png';
+
+
+  // update the round score IF the rolled number must not a 1
+  if (dice !== 1) {
+    roundScore += dice;
+    document.querySelector('#score-' + activePlayer).textContent = roundScore;
+    return roundScore;
+  } else {
+    document.querySelector('#score-' + activePlayer).textContent = 0;
+    roundScore = 0;
+    activePlayer = 1 - activePlayer;
+    return activePlayer;
+  };
+});
+
+document.querySelector('.btn-hold').addEventListener('click', function(){
+    
+});
